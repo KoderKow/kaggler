16edaa858877d201096571b6e8681b8cd2ce79ce
Kyle Harris <koderkow@gmail.com>
2021-08-28 11:32:55 -0500

    new method for storing competition info in _kaggle_data/meta

--- R/kgl_flow_meta.R
+++ R/kgl_flow_meta.R
@@ -30,3 +30,32 @@ kgl_flow_meta <- function() {
     usethis::ui_todo("metadata files does not exist. Do you need to execute {ui_kgl_flow}?")
   }
 }
+
+#' Show competition info
+#'
+#' This function will show some competition information for the current project.
+#'
+#' @return Compeition info.
+#' @export
+#' @family Kaggle Flows
+#'
+#' @examples
+#' \dontrun{
+#' library(kaggler)
+#'
+#' kgl_flow("titanic")
+#'
+#' kgl_flow_competition_info()
+#' }
+kgl_flow_competition_info <- function() {
+  dir_meta <- fs::path(.kgl_dir, "meta")
+  path_compeition_info <- fs::path(dir_meta, "competition_info")
+
+  if (fs::file_exists(path_compeition_info)) {
+    readRDS(path_compeition_info)
+  } else {
+    # ui_kgl_flow <- usethis::ui_value("kgl_flow()")
+    # usethis::ui_todo("metadata files does not exist. Do you need to execute {ui_kgl_flow}?")
+    usethis::ui_oops("File does not currently exist.")
+  }
+}
