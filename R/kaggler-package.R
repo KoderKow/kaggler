1ef015a3869e0855cfb182ca948e71209e869a47
Kyle Harris <koderkow@gmail.com>
2021-08-29 22:35:41 -0500

    silence package checking notes

--- R/kaggler-package.R
+++ R/kaggler-package.R
@@ -6,6 +6,6 @@
 #' @keywords internal
 "_PACKAGE"
 
-gv <- c(".", "download.file", "name", "ref", "submission_date", "unzip", "score", "browseURL", "name_old", "total_bytes", "check_path_competition_id")
+gv <- c(".", "download.file", "name", "ref", "submission_date", "unzip", "score", "browseURL", "name_old", "total_bytes", "check_path_competition_id", "kgl_home_dir")
 
 globalVariables(gv)
